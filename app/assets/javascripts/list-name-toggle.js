1908df381324aa44222425edd2653ba7cee11214
dorothy <dorothy.thur@gmail.com>
2015-02-20 10:42:42 -0500

    Fix issue with form field not clearing when canceling name update

--- app/assets/javascripts/list-name-toggle.js
+++ app/assets/javascripts/list-name-toggle.js
@@ -8,6 +8,8 @@ $(document).on('click','#list-name-form #cancel-edit-list-name', function () {
     $('#list-name-form').toggle();
     $('#edit-list-name').toggle();
     $('#list-name-current').toggle();
+    var oldName = $("#list-name-current").text();
+    $("#list_name").val(oldName);
   });
 
 $(document).ready(function() {
