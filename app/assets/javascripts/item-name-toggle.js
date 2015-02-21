178a8b53ce4cbb4905d4ad7282c9f58d741b54a2
dorothy <dorothy.thur@gmail.com>
2015-02-20 21:41:14 -0500

    Fix cancel button display toggle issues.

--- app/assets/javascripts/item-name-toggle.js
+++ app/assets/javascripts/item-name-toggle.js
@@ -2,6 +2,7 @@ $(document).on('click','.edit-item-name', function () {
     var parent = $(this).parent();
     $('.item_name_update_form', parent).toggle();
     $('.edit-item-name', parent).toggle();
+    $('.cancel-edit-item-name', parent).toggle();
     $('h3', parent).toggle();
   });
 
@@ -9,6 +10,7 @@ $(document).on('submit','.item_name_update_form', function () {
   var parent = $(this).parent();
   $('.item_name_update_form', parent).toggle();
   $('.edit-item-name', parent).toggle();
+  $('.cancel-edit-item-name', parent).toggle();
   $('h3').toggle();
 });
 
@@ -17,6 +19,7 @@ $(document).on('click','.cancel-edit-item-name', function () {
   $('.item_name_update_form', parent).toggle();
   $('.edit-item-name', parent).toggle();
   $('h3', parent).toggle();
+  $('.cancel-edit-item-name', parent).toggle();
   var oldName = $("h3", parent).text();
   $("#item_name", parent).val(oldName);
 });
--- app/assets/javascripts/list-name-toggle.js
+++ app/assets/javascripts/list-name-toggle.js
@@ -1,6 +1,7 @@
 $(document).on('click','#edit-list-name',function () {
     $('.list_name_update_form').toggle();
     $('#edit-list-name').toggle();
+    $('#cancel-edit-list-name').toggle();
     $('h2').toggle();
   });
 
@@ -8,12 +9,14 @@ $(document).on('click','#cancel-edit-list-name', function () {
     $('.list_name_update_form').toggle();
     $('#edit-list-name').toggle();
     $('h2').toggle();
+    $('#cancel-edit-list-name').toggle();
     var oldName = $("h2").text();
     $("#list_name").val(oldName);
   });
 
-$(document).on('click','.cancel-edit-list-name', function () {
+$(document).on('submit','.list_name_update_form', function () {
   $('.list_name_update_form').toggle();
   $('#edit-list-name').toggle();
   $('h2').toggle();
+  $('#cancel-edit-list-name').toggle();
 });
